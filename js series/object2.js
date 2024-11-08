db4e3cbac209246d1881937905cc6229702ca926
sharo-cod <sharo4192@gmail.com>
2024-11-08 21:27:47 +0500

    new

---
+++ js series/object2.js
@@ -0,0 +1,28 @@
+// let tinderUser = new Object()
+// OR
+let tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "SHARIQ ALAM"
+tinderUser.isLoggedIn = true
+
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email : "shariqalam@gmail.com",
+    fullName: {
+        userFullname : {
+            firstName: "Shariq ",
+            lastName : "Alam"
+        }
+
+    }
+}
+// console.log(regularUser.fullName.userFullname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+
+const obj3 = {obj1,obj2};
+console.log(obj3);
