8d9cc740991dab612798bf300ea3d808fd89c33e
sharo-cod <sharo4192@gmail.com>
2024-11-09 23:40:34 +0500

    new

--- js series/object2.js
+++ js series/object2.js
@@ -24,5 +24,6 @@ const regularUser = {
 const obj1 = {1: "a", 2: "b"}
 const obj2 = {3: "a", 4: "b"}
 
-const obj3 = {obj1,obj2};
+// const obj3 = {obj1,obj2};
+const obj4 = Object.assign()
 console.log(obj3);
