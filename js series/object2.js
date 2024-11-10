6555bf3419d01e3ef56014cc7af6498e634856cb
sharo-cod <sharo4192@gmail.com>
2024-11-10 22:45:53 +0500

    obj complete

--- js series/object2.js
+++ js series/object2.js
@@ -1,11 +1,11 @@
 // let tinderUser = new Object()
 // OR
-let tinderUser = {}
+let tinderUser = {
 
-tinderUser.id = "123abc"
-tinderUser.name = "SHARIQ ALAM"
-tinderUser.isLoggedIn = true
-
+tinderUserid : "123abc",
+tinderUsername : "SHARIQ ALAM",
+tinderUserisLoggedIn : true
+}
 
 // console.log(tinderUser);
 
@@ -23,7 +23,29 @@ const regularUser = {
 
 const obj1 = {1: "a", 2: "b"}
 const obj2 = {3: "a", 4: "b"}
+const obj4 = {5: "a", 6: "b"}
 
-// const obj3 = {obj1,obj2};
-const obj4 = Object.assign({},obj1 , obj2)
-console.log(obj4);
+const obj3 = {...obj1 ,...obj2};
+// const obj5 = Object.assign({},obj1 , obj2)
+console.log(obj3);
+
+
+
+
+
+// let abc = [1,2,3,4,5,6]
+// let def = [7,8,9,10]
+//                                               //        :)
+// let asj = Object.assign({} ,abc, def)
+// console.log(asj);
+
+console.log(tinderUser);
+console.log("---------------------------------------------------------------------");
+
+console.log(Object.values(tinderUser));
+console.log(Object.keys(tinderUser));
+console.log(Object.entries(tinderUser));       // MAKE ALL THE KEY VALUE AN ARRAY
+console.log(tinderUser.hasOwnProperty('tinderUserisLoggedIn'));  // TO CHECK TH VALUE
+console.log(tinderUser.hasOwnProperty('tindsdfsdfdsfsedIn'));  
+
+
