647b6ac2bed6c0351efb800ae6803b3f973df11e
sharo-cod <sharo4192@gmail.com>
2024-11-14 22:37:06 +0500

    COC JS CONTROLFLOW incomplete

---
+++ js series/controlflow.js
@@ -0,0 +1,49 @@
+// if
+
+if(true){
+                              // Will Execute 
+}
+
+if(false){
+                              // Will Not Execute
+}
+
+// =====================================================================
+
+/*
+
+[
+<  les then
+>  greater then
+<= less or equals 
+>= greater or equals
+=  operator asignment
+== equals to
+!= not equals
+=== equals to and also check the type i.e ( 2 === "2" ) it will also check the type of  this expression [ THIS IS ALSO KNOWN AS STRICED EQUAL].
+
+]
+*/
+
+
+// const Temperature = 41;
+// if(Temperature === 41){
+//    console.log("less then 50");
+// } else{
+//    console.log("Tmperatue is greater then 50");
+// }
+// console.log("Execute");
+
+
+
+const score = 200;
+if (score){
+   const power = "fly"
+   console.log(`User Power : ${power}`);
+}
+// console.log(`User Power : ${power}`);  it will not run because of scope instead it can run when you use the var
+
+
+
+const balance =  1000;
+if (balance > 500)  console.log("test"),console.log("test2"); // DONT WRITE A CODE LIKE THIS
