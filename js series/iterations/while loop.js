985e91b500f95cacdacedf6d0861de731d2060eb
sharo-cod <sharo4192@gmail.com>
2024-11-17 00:12:08 +0500

    while loop comlete

---
+++ js series/iterations/while loop.js
@@ -0,0 +1,29 @@
+//  WHILE LOOP
+
+// let index = 0;
+// while (index <= 10) {
+
+//     console.log(`VALUE OF INDEX IS ${index}`);
+
+//     index = index + 2
+    
+// }
+
+let myArray = ['Flash' , 'Batman' , 'Superman']
+
+// let Arr = 0
+// while (Arr < myArray.length) {
+
+//     console.log(`Value is ${myArray [Arr]}`);
+
+//     Arr++
+// }
+
+//  DO WHILE LOOP
+
+
+let score = 11
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10 );
