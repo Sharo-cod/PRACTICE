9806e6a2f58eaf4eb80df2cb4da9f89901a0aeaf
sharo-cod <sharo4192@gmail.com>
2024-12-06 00:06:15 +0500

    Complete

--- JS 30 Projecs/Button_Effect/button.js
+++ JS 30 Projecs/Button_Effect/button.js
@@ -1,9 +1,8 @@
-const btnEl = document.querySelector(".btn")
-
-btnEl.addEventListener("mouseover", (event) => {
-    const x = event.pageX - btnEl.offsetleft;
+const btnEl = document.querySelector('.btn');
+btnEl.addEventListener('mouseover',(event)=>{
+    const x = event.pageX - btnEl.offsetLeft; 
     const y = event.pageY - btnEl.offsetTop;
-
-    btnEl.style.setProperty("--xPos" , x + "px" )
-    btnEl.style.setProperty("--yPos" , y + "px" )
-});
+    
+    btnEl.style.setProperty("--xPos", x + "px")
+    btnEl.style.setProperty("--yPos", y + "px")
+})
